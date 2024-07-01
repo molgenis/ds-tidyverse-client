fd94b92f9c00707a6f63c8fe60f069c64b408713
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-07-01 13:41:27 +0200

    docs: gh pages (#35)
    * docs: added github pages
    
    * docs: renamed vignettes and updated title
    
    * docs: updated gh pages
    
    * docs: added additional contributors
    
    * chore: remove tests which should not be on this branch

--- vignettes/pre-render.R
+++ vignettes/pre-render.R
@@ -1,6 +1,6 @@
 # To update the vignette:
-knitr::knit("vignettes/select.Rmd.orig", output = "vignettes/select.Rmd")
+knitr::knit("vignettes/dstidyverse.Rmd.orig", output = "vignettes/dstidyverse.Rmd")
 
 # To create a script that uploads the files:
-knitr::purl("vignettes/select.Rmd.orig", output = "vignettes/select.R")
+knitr::purl("vignettes/dstidyverse.Rmd.orig", output = "vignettes/dstidyverse.R")
 
