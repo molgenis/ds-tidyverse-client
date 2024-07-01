c5d0fb696cd10a15ca65893187f7c71a3e3aa9bf
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-07-01 13:36:16 +0200

    feat: rename and mutate
    * refactor: simplified code as done with ds.select
    
    * first commit rename
    
    * fix: was mistakenly calling select
    
    * chore: passed tests
    
    * test: test that newobj is passed
    
    * docs: documented addtional functions
    
    * chore: install releated branch of serverside package
    
    * docs: updated readme
    
    * chore: fixing ci
    
    * docs: tweaked readme
    
    * refactor: moved general function to utils
    
    * feat: added mutate function
    
    * refactor: moved basic arg checks to utils
    
    * refactor: removed obselete argument
    
    * fix: no longer overwrite current df if not default supplied
    
    * test: updated tests for increase to permitted values
    
    * docs: added missing argument descriptions
    
    * docs: added mutate to docs
    
    * refactor: call generic function from utils
    
    * refactor: moved disclosure args to generic function
    
    * tests: updated to work with refactored code
    
    * fix: updated tests to allow new functions to pass
    
    * refactor: generalised call to serverside function
    
    * refactor: moved generalised function to utils
    
    * refactor: now used generalised function in other clientside functions
    
    * refactor: changed order to be more logical
    
    * refactor: moved checks and serverside call to one function
    
    * refactor: tried to give better function name
    
    * docs: added roxygen docs
    
    * refactor: split one function to two
    
    * refactor: moved back to a slightly less generalised solution to make code clearer and tests easier to write
    
    * fix: removed unneeded arguments
    
    * fix: had arguments in wrong order
    
    * test: improved test coverage
    
    * passed check
    
    * chore: updated install branch for serverside package
    
    * docs: updated readme to include mutate as implemented
    
    * refactor: now use dsBase disclosure function
    
    * chore: remove rogue trycatch

--- R/ds.rename.R
+++ R/ds.rename.R
@@ -1,8 +1,8 @@
-#' @title Clientside dplyr select function
-#' @description This function is similar to R function \code{select}.
-#' @details Performs dplyr select
+#' @title Clientside dplyr rename function
+#' @description This function is similar to R function \code{rename}.
+#' @details Performs dplyr rename
 #' @param df.name Character specifying a serverside data frame or tibble.
-#' @param tidy_select List of Tidyselect syntax to be passed to dplyr::select.
+#' @param tidy_select List of Tidyselect syntax to be passed to dplyr::rename
 #' @param newobj Optionally, character specifying name for new server-side data frame. Default is
 #' to overwrite original object.
 #' @param datasources datashield connections object.
@@ -10,10 +10,10 @@
 #' as default same name as input object is written to the serverside.
 #' @importFrom DSI datashield.assign datashield.aggregate
 #' @export
-ds.select <- function(df.name = NULL, tidy_select = NULL, newobj = NULL, datasources = NULL) {
+ds.rename <- function(df.name = NULL, tidy_select = NULL, newobj = NULL, datasources = NULL) {
   tidy_select <- .format_args_as_string(rlang::enquo(tidy_select))
   datasources <- .set_datasources(datasources)
   .perform_tidyverse_checks(df.name, newobj, tidy_select, datasources)
-  cally <- .make_serverside_call("selectDS", df.name, tidy_select, NULL)
+  cally <- .make_serverside_call("renameDS", df.name, tidy_select, NULL)
   datashield.assign(datasources, newobj, cally)
 }
