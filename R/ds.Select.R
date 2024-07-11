2695ac8cb02b68c8e7a3774513957e19753cd8a6
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-07-11 10:46:04 +0200

    chore: changed file to lower case

--- R/ds.Select.R
+++ R/ds.Select.R
@@ -1,14 +1,15 @@
 #' @title Clientside dplyr select function
-#' @description This function is similar to R function \code{select}.
+#' @description This function is similar to R function \code{dplyr::select}.
 #' @details Performs dplyr select
 #' @param df.name Character specifying a serverside data frame or tibble.
 #' @param tidy_select List of Tidyselect syntax to be passed to dplyr::select.
 #' @param newobj Optionally, character specifying name for new server-side data frame. Default is
 #' to overwrite original object.
 #' @param datasources datashield connections object.
-#' @return the object specified by the \code{newobj} argument or
+#' @return The object specified by the \code{newobj} argument or
 #' as default same name as input object is written to the serverside.
 #' @importFrom DSI datashield.assign datashield.aggregate
+#' @example Refer to the package vignette for examples
 #' @export
 ds.select <- function(df.name = NULL, tidy_select = NULL, newobj = NULL, datasources = NULL) {
   tidy_select <- .format_args_as_string(rlang::enquo(tidy_select))
