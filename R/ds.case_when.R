a39b4d31bfe9441b1dd06f4a720937386cc1098c
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-07-11 14:19:33 +0200

    docs: fix example and rename argument

--- R/ds.case_when.R
+++ R/ds.case_when.R
@@ -1,6 +1,13 @@
 #' @title A general vectorised if-else
 #' @description DataSHIELD implementation of \code{dplyr::case_when}.
-#' @param tidy_select
+#' @param dynamic_dots A sequence of two-sided formulas. The left hand side (LHS) determines which
+#' values match this case. The right hand side (RHS) provides the replacement value.
+#' The LHS inputs must evaluate to logical vectors.
+#' The RHS inputs will be coerced to their common type.
+#' All inputs will be recycled to their common size. That said, we encourage all LHS inputs to be
+#' the same size. Recycling is mainly useful for RHS inputs, where you might supply a size 1 input
+#' that will be recycled to the size of the LHS inputs.
+#' NULL inputs are ignored.
 #' @param default The value used when all of the LHS inputs return either FALSE or NA.
 #' @param .ptype An optional prototype declaring the desired output type. If supplied, this overrides the common type of true, false, and missing.
 #' @param .size An optional size declaring the desired output size. If supplied, this overrides the size of condition.
@@ -14,23 +21,21 @@
 #'
 #'ds.case_when(
 #' tidy_select = list(
-#'   case_when(
 #'     mtcars$mpg < 10 ~ "low",
 #'     mtcars$mpg >= 10 & mtcars$mpg < 20 ~ "medium",
-#'     mtcars$mpg >= 20 ~ "high")
-#'     ),
+#'     mtcars$mpg >= 20 ~ "high"),
 #'   newobj = "test",
 #'   datasources = conns)
 #'
 #' ## Refer to the package vignette for more examples.
 #' }
 #' @export
-ds.case_when <- function(tidy_select = NULL, default = NULL, .ptype = NULL, .size = NULL,
+ds.case_when <- function(dynamic_dots = NULL, default = NULL, .ptype = NULL, .size = NULL,
                        newobj = NULL, datasources = NULL) {
-  tidy_select <- .format_args_as_string(rlang::enquo(tidy_select))
+  dynamic_dots <- .format_args_as_string(rlang::enquo(dynamic_dots))
   datasources <- .set_datasources(datasources)
-  .perform_tidyverse_checks(df.name = NULL, newobj, tidy_select, datasources, check_df = FALSE)
-  cally <- .make_serverside_call("case_whenDS", tidy_select, list(default, .ptype, .size))
+  .perform_tidyverse_checks(df.name = NULL, newobj, dynamic_dots, datasources, check_df = FALSE)
+  cally <- .make_serverside_call("case_whenDS", dynamic_dots, list(default, .ptype, .size))
   datashield.assign(datasources, newobj, cally)
 }
 
