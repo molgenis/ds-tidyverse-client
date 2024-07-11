ffa456b987419cb70033c0229dad7e524ef90ee2
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-07-11 11:12:04 +0200

    docs: added examples, cleaned up help files

--- R/ds.rename.R
+++ R/ds.rename.R
@@ -1,17 +1,12 @@
-#' @title Clientside dplyr rename function
-#' @description This function is similar to R function \code{rename}.
-#' @details Performs dplyr rename
+#' @title Rename columns.
+#' @description DataSHIELD implentation of \code{dplyr::rename}.
 #' @param df.name Character specifying a serverside data frame or tibble.
-#' @param tidy_select List of Tidyselect syntax to be passed to dplyr::rename
-#' @param newobj Optionally, character specifying name for new server-side data frame. Default is
-#' to overwrite original object.
-#' @param datasources datashield connections object.
-#' @return the object specified by the \code{newobj} argument or
-#' as default same name as input object is written to the serverside.
+#' @param tidy_select List of tidyselect syntax to be passed to \code{dplyr::rename}.
+#' @param newobj Character specifying name for new server-side data frame.
+#' @param datasources DataSHIELD connections object.
+#' @return An object with the name specified by the \code{newobj} argument is written serverside.
 #' @importFrom DSI datashield.assign datashield.aggregate
-#' @examples
-#'\dontrun{
-#' ## First log in to a DataSHIELD session with mtcars dataset loaded.
+#' @examples First log in to a DataSHIELD session with \code{mtcars} dataset loaded.
 #'
 #' ds.rename(
 #'  df.name = "mtcars",
@@ -19,8 +14,7 @@
 #'  newobj = "df_renamed",
 #'  dataources = conns)
 #'
-#' ## Refer to the package vignette for more examples.
-#' }
+#' Refer to the package vignette for more examples.
 #' @export
 ds.rename <- function(df.name = NULL, tidy_select = NULL, newobj = NULL, datasources = NULL) {
   tidy_select <- .format_args_as_string(rlang::enquo(tidy_select))
--- R/ds.select.R
+++ R/ds.select.R
@@ -1,15 +1,20 @@
-#' @title Clientside dplyr select function
-#' @description This function is similar to R function \code{dplyr::select}.
-#' @details Performs dplyr select
+#' @title Keep or drop columns using their names and types.
+#' @description DataSHIELD implentation of  \code{dplyr::select}.
 #' @param df.name Character specifying a serverside data frame or tibble.
-#' @param tidy_select List of Tidyselect syntax to be passed to dplyr::select.
-#' @param newobj Optionally, character specifying name for new server-side data frame. Default is
-#' to overwrite original object.
-#' @param datasources datashield connections object.
-#' @return The object specified by the \code{newobj} argument or
-#' as default same name as input object is written to the serverside.
+#' @param tidy_select List of tidyselect syntax to be passed to  \code{dplyr::select}.
+#' @param newobj Character specifying name for new server-side data frame.
+#' @param datasources DataSHIELD connections object.
+#' @return An object with the name specified by the \code{newobj} argument is written serverside.
 #' @importFrom DSI datashield.assign datashield.aggregate
-#' @example Refer to the package vignette for examples
+#' @examples First log in to a DataSHIELD session with \code{mtcars} dataset loaded.
+#'
+#' ds.select(
+#'  df.name = "mtcars",
+#'  tidy_select = list(mpg, starts_with("t"),
+#'  newobj = "df_subset",
+#'  dataources = conns)
+#'
+#' Refer to the package vignette for more examples.
 #' @export
 ds.select <- function(df.name = NULL, tidy_select = NULL, newobj = NULL, datasources = NULL) {
   tidy_select <- .format_args_as_string(rlang::enquo(tidy_select))
