2ded538b907d7518e18483ef7bbecfe4fa36f467
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-07-23 13:38:21 +0200

    docs: added examples for all functions (#49)
    * docs: added examples for all functions
    
    * chore: updated CI serverside ref

--- R/ds.Select.R
+++ R/ds.Select.R
@@ -9,7 +9,18 @@
 #' @return The object specified by the \code{newobj} argument or
 #' as default same name as input object is written to the serverside.
 #' @importFrom DSI datashield.assign datashield.aggregate
-#' @example Refer to the package vignette for examples
+#' @examples
+#'\dontrun{
+#' ## First log in to a DataSHIELD session with mtcars dataset loaded.
+#'
+#' ds.select(
+#'  df.name = "mtcars",
+#'  tidy_select = list(mpg, starts_with("t"),
+#'  newobj = "df_subset",
+#'  dataources = conns)
+#'
+#' ## Refer to the package vignette for more examples.
+#' }
 #' @export
 ds.select <- function(df.name = NULL, tidy_select = NULL, newobj = NULL, datasources = NULL) {
   tidy_select <- .format_args_as_string(rlang::enquo(tidy_select))
--- R/ds.rename.R
+++ R/ds.rename.R
@@ -9,6 +9,18 @@
 #' @return the object specified by the \code{newobj} argument or
 #' as default same name as input object is written to the serverside.
 #' @importFrom DSI datashield.assign datashield.aggregate
+#' @examples
+#'\dontrun{
+#' ## First log in to a DataSHIELD session with mtcars dataset loaded.
+#'
+#' ds.rename(
+#'  df.name = "mtcars",
+#'  tidy_select = list(new_var_1 = mpg, new_var_2 = cyl),
+#'  newobj = "df_renamed",
+#'  dataources = conns)
+#'
+#' ## Refer to the package vignette for more examples.
+#' }
 #' @export
 ds.rename <- function(df.name = NULL, tidy_select = NULL, newobj = NULL, datasources = NULL) {
   tidy_select <- .format_args_as_string(rlang::enquo(tidy_select))
