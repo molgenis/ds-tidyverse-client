29ecbf05264dc07ac367bf96d5f2469ef3c22273
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-08-26 16:53:47 +0200

    feat: first commit group_by

---
+++ R/ds.group_by.R
@@ -0,0 +1,24 @@
+#' @title Group by one or more variables
+#' @description DataSHIELD implentation of  \code{dplyr::group_by}.
+#' @param df.name Character specifying a serverside data frame or tibble.
+#' @param expr Variables or computations to group by.
+#' @param .add 	When FALSE, the default, group_by() will override existing groups. To add to the
+#' existing groups, use .add = TRUE.
+#' @param .drop Drop groups formed by factor levels that don't appear in the data? The default is
+#' TRUE except when .data has been previously grouped with .drop = FALSE.
+#' @param newobj Character specifying name for new server-side data frame.
+#' @param datasources DataSHIELD connections object.
+#' @return An object with the name specified by the \code{newobj} argument is written serverside.
+#' @importFrom DSI datashield.assign datashield.aggregate
+#' @examples
+#'\dontrun{
+#'  ds.filter(
+#' }
+#' @export
+ds.group_by <- function(df.name = NULL, expr, .add = NULL, .drop = NULL, newobj = NULL, datasources = NULL) {
+  tidy_select <- .format_args_as_string(rlang::enquo(expr))
+  datasources <- .set_datasources(datasources)
+  .perform_tidyverse_checks(df.name, newobj, tidy_select, datasources)
+  cally <- .make_serverside_call("filterDS", tidy_select, list(df.name, .add, .drop))
+  datashield.assign(datasources, newobj, cally)
+}
