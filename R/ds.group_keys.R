569e501672551f12a907774be9ad340148ed14cb
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-08-29 16:24:22 +0200

    refactor: specify null arguments in subfunction calls

--- R/ds.group_keys.R
+++ R/ds.group_keys.R
@@ -1,8 +1,8 @@
-#' @title Describing the groups within a grouped tibble
+#' @title Describe the groups within a grouped tibble
 #' @description DataSHIELD implentation of \code{dplyr::group_keys}.
 #' @param df.name Character specifying a serverside tibble.
 #' @param datasources DataSHIELD connections object.
-#' @return A tibble describing the groups
+#' @return A tibble describing the groups.
 #' @importFrom DSI datashield.assign datashield.aggregate
 #' @examples
 #'\dontrun{
@@ -11,7 +11,7 @@
 #' @export
 ds.group_keys <- function(df.name = NULL, datasources = NULL) {
   datasources <- .set_datasources(datasources)
-  .perform_tidyverse_checks(df.name, datasources)
-  cally <- .make_serverside_call("groupKeysDS", list(df.name))
-  datashield.aggregate(datasources, newobj, cally)
+  .perform_tidyverse_checks(df.name, newobj = NULL, tidy_select = NULL, datasources, check_obj = F)
+  cally <- .make_serverside_call("groupKeysDS", tidy_select = NULL, list(df.name))
+  datashield.aggregate(datasources, cally)
 }
