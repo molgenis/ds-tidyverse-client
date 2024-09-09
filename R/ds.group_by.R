c59788decc1356d90704cd94506e062bcc2177a1
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-09-09 15:53:54 +0200

    docs: tidied up

--- R/ds.group_by.R
+++ R/ds.group_by.R
@@ -2,7 +2,7 @@
 #' @description DataSHIELD implentation of \code{dplyr::group_by}.
 #' @param df.name Character specifying a serverside data frame or tibble.
 #' @param expr Variables or computations to group by.
-#' @param .add 	When FALSE, the default, group_by() will override existing groups. To add to the
+#' @param .add 	When FALSE, the default, \code{group_by()} will override existing groups. To add to the
 #' existing groups, use .add = TRUE.
 #' @param .drop Drop groups formed by factor levels that don't appear in the data? The default is
 #' TRUE except when .data has been previously grouped with .drop = FALSE.
@@ -28,7 +28,7 @@ ds.group_by <- function(df.name = NULL, expr, .add = FALSE, .drop = TRUE, newobj
 
 #' @title Remove grouping
 #' @description DataSHIELD implentation of \code{dplyr::ungroup}.
-#' @param x a tbl.
+#' @param x a tibble.
 #' @param newobj Character specifying name for new server-side data frame.
 #' @param datasources DataSHIELD connections object.
 #' @examples
