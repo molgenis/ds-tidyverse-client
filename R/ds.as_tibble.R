f3aea1713ff0e3dacaec60e565f580a16b424eab
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-09-12 19:58:09 +0200

    feat: first commit as_tibble

---
+++ R/ds.as_tibble.R
@@ -0,0 +1,32 @@
+#' @title Coerce a data frame or matrix to a tibble. Currently not implemented for lists or other
+#' object types.
+#' @description DataSHIELD implementation of \code{tibble::as_tibble}.
+#' @param x A data frame or matrix.
+#' @param .rows The number of rows, useful to create a 0-column tibble or just as an additional
+#' check.
+#' @param .name_repair Treatment of problematic column names:
+#'   * "minimal": No name repair or checks, beyond basic existence,\
+#'   * "unique": Make sure names are unique and not empty,
+#'   * "check_unique": (default value), no name repair, but check they are unique,
+#'   * "universal": Make the names unique and syntactic
+#' @param rownames How to treat existing row names of a data frame or matrix:
+#'   * `NULL`: remove row names. This is the default.
+#'   * `NA`: keep row names.
+#'   * A string: the name of a new column. Existing rownames are transferred
+#'     into this column and the `row.names` attribute is deleted.
+#'     No name repair is applied to the new column name, even if `x` already contains
+#'     a column of that name.
+#' @param column_name Optionally, specify column names of the new object.
+#' @return A serverside tibble with the name specified in the \code{newobj}.
+#' @importFrom DSI datashield.assign datashield.aggregate
+#' @examples
+#'\dontrun{
+#' }
+#' @export
+ds.as_tibble <- function(x = NULL, .rows = NULL, .name_repair = NULL, rownames = NULL,
+                         column_name = NULL, newobj = NULL, datasources = NULL) {
+  datasources <- .set_datasources(datasources)
+  .perform_tidyverse_checks(df.name = NULL, newobj, datasources)
+  cally <- .make_serverside_call("asTibbleDS", list(.rows, .name_repair, column_name))
+  datashield.assign(datasources, newobj, cally)
+}
