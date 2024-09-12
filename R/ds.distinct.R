dac3b4a8a5337a31781893ab72ed6c5a7e3019d0
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-09-12 18:11:15 +0200

    docs: tidied up documentation and added example

--- R/ds.distinct.R
+++ R/ds.distinct.R
@@ -4,14 +4,18 @@
 #' @param expr Optional variables to use when determining uniqueness. If there are multiple rows
 #' for a given combination of inputs, only the first row will be preserved. If omitted, will use all
 #' variables in the data frame.
-#' @param .keep_all If TRUE, keep all variables in .data. If a combination of ... is not distinct,
-#' this keeps the first row of values.
+#' @param .keep_all If TRUE, keep all variables in .data. If a combination of `expr` is not
+#' distinct, this keeps the first row of values.
 #' @param newobj Character specifying name for new server-side data frame.
 #' @param datasources DataSHIELD connections object.
 #' @return An object with the name specified by the \code{newobj} argument is written serverside.
 #' @importFrom DSI datashield.assign datashield.aggregate
 #' @examples
 #'\dontrun{
+#'ds.distinct(
+#' df.name = "mtcars",
+#' expr = list(mpg, cyl),
+#' newobj = "distinct_df")
 #' }
 #' @export
 ds.distinct <- function(df.name = NULL, expr = NULL, .keep_all = FALSE, newobj = NULL,
