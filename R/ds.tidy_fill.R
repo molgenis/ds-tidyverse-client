3ced0af493a6ce0a5fa1f686755e506baa25caf0
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-09-26 11:54:30 +0200

    refactor: tidying everything up

--- R/ds.tidy_fill.R
+++ R/ds.tidy_fill.R
@@ -18,31 +18,32 @@
 #'  )
 #' }
 #' @export
-ds.tidy_fill <- function(df.name = NULL, fix_class = NULL, newobj = NULL, datasources = NULL) {
-  browser()
+ds.tidy_fill <- function(df.name = NULL, fix_class = FALSE, newobj = NULL, datasources = NULL) {
+
   datasources <- .set_datasources(datasources)
   # .perform_tidyverse_checks(df.name, newobj, tidy_select, datasources)
+  browser()
 
-  unique_cols <- .get_unique_cols(df.name, datasources)
-  .make_cols_same(df.name, unique_cols, newobj, datasources)
+  col_names <- datashield.aggregate(datasources, call("colnamesDS", df.name))
+  .stop_if_cols_identical(col_names)
 
   var_classes <- .get_var_classes(df.name, datasources)
-  classes_neat <- .format_var_classes(var_classes)
-  different_class <- .identify_class_conflict(classes_neat)
+  different_classes <- .identify_class_conflicts(var_classes)
+  .stop_if_class_problems(different_classes, fix_class)
 
-  if(length(different_class) > 0 & fix_class == F) {
-    cli_abort("The following variables have conflicting classes for different servers and
-              `fix_class` is set to false: {names(different_class)}")
-  }
+  unique_cols <- .get_unique_cols(col_names)
+  .add_missing_cols_to_df(df.name, unique_cols, newobj, datasources)
 
-  target_vars <- names(different_class)
-  target_classes <- different_class %>% map_chr(~.[[1]])
 
-  na_vars <- classes_neat[classes_neat %>% map_lgl(~any(is.na(.)))]
+
+
+  target_vars <- names(different_classes)
+  target_classes <- different_classes %>% map_chr(~.[[1]])
+
+  na_vars <- var_classes[var_classes %>% map_lgl(~any(is.na(.)))]
   na_class <- as.character(na_vars[1, ])
 
   cally <- call("fixClassDS", newobj, c(target_vars, names(na_vars)), c(unname(target_classes), na_class))
-
   datashield.assign(datasources, newobj, cally)
 
 
@@ -51,57 +52,78 @@ ds.tidy_fill <- function(df.name = NULL, fix_class = NULL, newobj = NULL, dataso
     select(where(~ . == "factor"))
 
   cally <- call("getAllLevelsDS", newobj, names(factor_vars))
+  factor_levels <- datashield.aggregate(datasources, cally)
+
+  level_match <- factor_levels %>%
+    pmap_lgl(function(...) {
+      args <- list(...)
+      all(map_lgl(args[-1], ~ identical(.x, args[[1]])))
+    })
+
+  unique_levels <- factor_levels %>%
+    map(~.[!level_match]) %>%
+    pmap(function(...) {
+
+      as.character(c(...))
+
+    }) %>%
+    map(~unique(.))
+
+  cally <- call("setAllLevelsDS", newobj, names(unique_levels), unique_levels)
   datashield.assign(datasources, newobj, cally)
 
-
-
-
-  levelsAllColsDS
-
-
-  browser()
 }
 
 
-
-
-
-
-  .identify_class_conflict <- function(classes_neat) {
-
-    different_class <- classes_neat |>
-      dplyr::select(-server) |>
-      map(~unique(na.omit(.)))
-
-    out <- different_class[which(different_class %>% map(length) > 1)]
-    return(out)
+.stop_if_cols_identical <- function(col_names) {
+  are_identical <- Reduce(identical, col_names)
+  if(are_identical) {
+    cli_abort("Columns are identical in all data frames: nothing to fill")
   }
+}
 
-
-  .format_var_classes <- function(classes_neat) {
-    out <- classes_neat |>
-      map(as_tibble) |>
-      bind_rows(.id = "server")
-    return(out)
-  }
-
+.get_unique_cols <- function(col_names){
+  return(
+    unique(
+      unlist(col_names))
+  )
+}
 
 .get_var_classes <- function(df.name, datasources) {
   cally <- call("classAllColsDS", df.name)
-  classes <- datashield.aggregate(datasources, cally)
+  classes <- datashield.aggregate(datasources, cally) %>%
+    bind_rows(.id = "server")
   return(classes)
 }
 
-.make_cols_same <- function(df.name, unique_cols, newobj, datasources) {
-cally <- call("makeColsSameDS", df.name, unique_cols)
-datashield.assign(datasources, newobj, cally)
+.add_missing_cols_to_df <- function(df.name, unique_cols, newobj, datasources) {
+  cally <- call("makeColsSameDS", df.name, unique_cols)
+  datashield.assign(datasources, newobj, cally)
 
 }
 
-.get_unique_cols <- function(df.name, datasources){
-  cols <- datashield.aggregate(datasources, call("colnamesDS", df.name))
-  return(
-    unique(
-      unlist(cols))
-  )
+.identify_class_conflicts <- function(classes) {
+
+  different_class <- classes |>
+    dplyr::select(-server) |>
+    map(~unique(na.omit(.)))
+
+  out <- different_class[which(different_class %>% map(length) > 1)]
+  return(out)
 }
+
+  .stop_if_class_problems <- function(different_classes, fix_class) {
+
+    if(length(different_classes) > 0 & fix_class == F) {
+      cli_abort(
+        c(
+          "x" = "Cannot fill columns where `fix_class` is FALSE and classes are not identical for all servers",
+          "i" = "fix_class is FALSE",
+          "i" = "Variables {names(different_classes)} do not share the same class"
+        )
+      )
+    }
+  }
+
+
+
