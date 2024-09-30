3f9844fcc80e0b049ae3bd1ca00a6c180d5d3a41
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-09-30 12:54:14 +0200

    chore: refactor tests, no longer use helper function as not compatible with other refactor

--- tests/testthat/test-rename.R
+++ tests/testthat/test-rename.R
@@ -4,26 +4,18 @@ library(dsTidyverse)
 library(dsBase)
 library(dsBaseClient)
 
-options(datashield.env = environment())
-data("mtcars")
-dslite.server <- DSLite::newDSLiteServer(tables = list(mtcars = mtcars))
-data("logindata.dslite.cnsim")
-logindata.dslite.cnsim <- logindata.dslite.cnsim %>%
-  mutate(table = "mtcars")
-dslite.server$config(defaultDSConfiguration(include = c("dsBase", "dsTidyverse")))
-dslite.server$assignMethod("renameDS", "dsTidyverse::renameDS")
-dslite.server$aggregateMethod("exists", "base::exists")
-dslite.server$aggregateMethod("classDS", "dsBase::classDS")
-dslite.server$aggregateMethod("lsDS", "dsBase::lsDS")
-dslite.server$aggregateMethod("dsListDisclosureSettingsTidyVerse", "dsTidyverse::dsListDisclosureSettingsTidyVerse")
-conns <- datashield.login(logins = logindata.dslite.cnsim, assign = TRUE)
+data(mtcars)
+login_data <- .prepare_dslite(assign_method = "bindRowsDS", tables = list(mtcars = mtcars))
+conns <- datashield.login(logins = login_data)
+datashield.assign.table(conns, "mtcars", "mtcars")
 
 test_that("ds.rename fails with correct error message if data not present ", {
   expect_error(
     ds.rename(
       df.name = "datanotthere",
       tidy_select = list(test_1 = mpg, test_2 = drat),
-      newobj = "nodata"
+      newobj = "nodata",
+      datasources = conns
     )
   )
 })
@@ -32,10 +24,15 @@ test_that("ds.rename correctly passes =", {
   ds.rename(
     df.name = "mtcars",
     tidy_select = list(test_1 = mpg, test_2 = drat),
-    newobj = "mpg_drat"
+    newobj = "mpg_drat",
+    datasources = conns
   )
-  expected <- c("test_1", "cyl", "disp", "hp", "test_2", "wt", "qsec", "vs", "am", "gear", "carb")
-  .check_cols_as_expected(expected, "mpg_drat")
+
+  expect_equal(
+    ds.colnames("mpg_drat", datasources = conns)[[1]],
+    c("test_1", "cyl", "disp", "hp", "test_2", "wt", "qsec", "vs", "am", "gear", "carb")
+  )
+
 })
 
 test_that("ds.rename throws an error if column name doesn't exist", {
@@ -43,7 +40,8 @@ test_that("ds.rename throws an error if column name doesn't exist", {
     ds.rename(
       df.name = "mtcars",
       tidy_select = list(test_1 = doesntexist, test_2 = drat),
-      newobj = "mpg_drat"
+      newobj = "mpg_drat",
+      datasources = conns
     )
   )
 })
