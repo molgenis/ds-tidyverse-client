797520941f84dbdb3e9163df7b1d674ae7ca5126
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-09-30 13:15:04 +0200

    refactor: remove helper method which is no longer used

--- tests/testthat/test-select.R
+++ tests/testthat/test-select.R
@@ -4,174 +4,242 @@ library(dsTidyverse)
 library(dsBase)
 library(dsBaseClient)
 
-options(datashield.env = environment())
 data("mtcars")
-dslite.server <- DSLite::newDSLiteServer(tables = list(mtcars = mtcars))
-data("logindata.dslite.cnsim")
-logindata.dslite.cnsim <- logindata.dslite.cnsim %>%
-  mutate(table = "mtcars")
-dslite.server$config(defaultDSConfiguration(include = c("dsBase", "dsTidyverse", "dsDanger")))
-dslite.server$assignMethod("selectDS", "dsTidyverse::selectDS")
-dslite.server$aggregateMethod("exists", "base::exists")
-dslite.server$aggregateMethod("classDS", "dsBase::classDS")
-dslite.server$aggregateMethod("lsDS", "dsBase::lsDS")
-dslite.server$aggregateMethod("dsListDisclosureSettingsTidyVerse", "dsTidyverse::dsListDisclosureSettingsTidyVerse")
-conns <- datashield.login(logins = logindata.dslite.cnsim, assign = TRUE)
+login_data <- .prepare_dslite(assign_method = "bindRowsDS", tables = list(mtcars = mtcars))
+conns <- datashield.login(logins = login_data)
+datashield.assign.table(conns, "mtcars", "mtcars")
 
-# test_that("ds.select fails with correct error message if data not present ", {
-#   expect_snapshot(
-#     ds.select(
-#       df.name = "datanotthere",
-#       tidy_select = list(mpg:drat),
-#       newobj = "nodata"))
-#   })## Wait until DSI updated
+test_that("ds.select fails with correct error message if data not present ", {
+  expect_error(
+    ds.select(
+      df.name = "datanotthere",
+      tidy_select = list(mpg:drat),
+      newobj = "nodata",
+      datasources = conns)
+    )
+})
 
 test_that("ds.select correctly passes : ", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list(mpg:drat),
-    newobj = "mpg_drat"
+    newobj = "mpg_drat",
+    datasources = conns
   )
-  expected <- c("mpg", "cyl", "disp", "hp", "drat")
-  .check_cols_as_expected(expected, "mpg_drat")
+
+  expect_equal(
+    ds.colnames("mpg_drat", datasources = conns)[[1]],
+    c("mpg", "cyl", "disp", "hp", "drat")
+  )
+
 })
 
 test_that("ds.select correctly passes `starts_with`", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list(starts_with("m")),
-    newobj = "starts"
+    newobj = "starts",
+    datasources = conns
   )
-  expected <- "mpg"
-  .check_cols_as_expected(expected, "starts")
+
+  expect_equal(
+    ds.colnames("starts", datasources = conns)[[1]],
+    "mpg"
+  )
+
 })
 
 test_that("ds.select correctly passes `ends_with`", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list(ends_with("m")),
-    newobj = "ends"
+    newobj = "ends",
+    datasources = conns
   )
-  expected <- "am"
-  .check_cols_as_expected(expected, "ends")
+
+  expect_equal(
+    ds.colnames("ends", datasources = conns)[[1]],
+    "am"
+  )
+
 })
 
 test_that("ds.select correctly passes `matches`", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list(matches("[aeiou]")),
-    newobj = "matches"
+    newobj = "matches",
+    datasources = conns
   )
-  expected <- c("disp", "drat", "qsec", "am", "gear", "carb")
-  .check_cols_as_expected(expected, "matches")
+
+  expect_equal(
+    ds.colnames("matches", datasources = conns)[[1]],
+    c("disp", "drat", "qsec", "am", "gear", "carb")
+  )
+
 })
 
 test_that("ds.select correctly passes `everything`", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list(everything()),
-    newobj = "everything"
+    newobj = "everything",
+    datasources = conns
   )
-  expected <- colnames(mtcars)
-  .check_cols_as_expected(expected, "everything")
+
+  expect_equal(
+    ds.colnames("everything", datasources = conns)[[1]],
+    colnames(mtcars)
+  )
+
 })
 
 test_that("ds.select correctly passes `last_col`", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list(last_col()),
-    newobj = "last"
+    newobj = "last",
+    datasources = conns
   )
-  expected <- "carb"
-  .check_cols_as_expected(expected, "last")
+
+  expect_equal(
+    ds.colnames("last", datasources = conns)[[1]],
+    "carb"
+  )
+
 })
 
 test_that("ds.select correctly passes `group_cols`", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list(group_cols()),
-    newobj = "group"
+    newobj = "group",
+    datasources = conns
   )
-  expected <- character(0)
-  .check_cols_as_expected(expected, "group")
+
+  expect_equal(
+    ds.colnames("group", datasources = conns)[[1]],
+    character(0)
+  )
+
 })
 
 test_that("ds.select correctly passes strings with '&'", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list(starts_with("c") & ends_with("b")),
-    newobj = "and"
+    newobj = "and",
+    datasources = conns
   )
-  expected <- "carb"
-  .check_cols_as_expected(expected, "and")
+
+  expect_equal(
+    ds.colnames("and", datasources = conns)[[1]],
+    "carb"
+  )
+
 })
 
 test_that("ds.select correctly passes strings with '!'", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list(!mpg),
-    newobj = "not"
+    newobj = "not",
+    datasources = conns
   )
-  expected <- c("cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb")
-  .check_cols_as_expected(expected, "not")
+
+  expect_equal(
+    ds.colnames("not", datasources = conns)[[1]],
+    c("cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb")
+  )
+
 })
 
 test_that("ds.select correctly passes strings with '|'", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list(starts_with("c") | ends_with("b")),
-    newobj = "or"
+    newobj = "or",
+    datasources = conns
   )
-  expected <- c("cyl", "carb")
-  .check_cols_as_expected(expected, "or")
+
+  expect_equal(
+    ds.colnames("or", datasources = conns)[[1]],
+    c("cyl", "carb")
+  )
+
 })
 
 test_that("ds.select correctly passes `strings with `all_of`", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list(all_of(c("mpg", "cyl"))),
-    newobj = "all_of"
+    newobj = "all_of",
+    datasources = conns
   )
-  expected <- c("mpg", "cyl")
-  .check_cols_as_expected(expected, "all_of")
+
+  expect_equal(
+    ds.colnames("all_of", datasources = conns)[[1]],
+    c("mpg", "cyl")
+  )
+
 })
 
 test_that("ds.select correctly passes strings with `any_of`", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list(any_of(c("mpg", "cyl"))),
-    newobj = "any_of"
+    newobj = "any_of",
+    datasources = conns
   )
-  expected <- c("mpg", "cyl")
-  .check_cols_as_expected(expected, "any_of")
+
+  expect_equal(
+    ds.colnames("any_of", datasources = conns)[[1]],
+    c("mpg", "cyl")
+  )
+
 })
 
 test_that("ds.select correctly passes complex strings", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list((starts_with("c") & ends_with("b")) | contains("ra") | gear:carb),
-    newobj = "complex"
+    newobj = "complex",
+    datasources = conns
   )
-  expected <- c("carb", "drat", "gear")
-  .check_cols_as_expected(expected, "complex")
+
+  expect_equal(
+    ds.colnames("complex", datasources = conns)[[1]],
+    c("carb", "drat", "gear")
+  )
+
 })
 
 test_that("ds.select correctly passes strings with `where`", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list(where(is.numeric)),
-    newobj = "where"
+    newobj = "where",
+    datasources = conns
   )
-  expected <- colnames(mtcars)
-  .check_cols_as_expected(expected, "where")
+
+  expect_equal(
+    ds.colnames("where", datasources = conns)[[1]],
+    colnames(mtcars)
+  )
+
 })
 
 test_that("ds.select correctly passes strings with '='", {
   ds.select(
     df.name = "mtcars",
     tidy_select = list(test = mpg, cyl, gear),
-    newobj = "equals"
+    newobj = "equals",
+    datasources = conns
   )
-  expected <- c("test", "cyl", "gear")
-  .check_cols_as_expected(expected, "equals")
+
+  expect_equal(
+    ds.colnames("equals", datasources = conns)[[1]],
+    c("test", "cyl", "gear")
+  )
+
 })
