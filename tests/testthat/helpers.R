41c33440f3659e3d1f35c543a7f855e6d1c67c1b
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-09-30 12:55:06 +0200

    refactor: remove helper method which is no longer used

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -1,19 +1,3 @@
-#' Check Columns as Expected
-#'
-#' Helper function for test files.
-#'
-#' @param expected A character vector of expected column names.
-#' @param df A dataframe whose columns are to be checked.
-#' @return This function does not return a value. It is used for its side effect of throwing an
-#' error if the observed column names do not match the expected ones.
-#' @importFrom dsBaseClient ds.colnames
-#' @importFrom testthat expect_equal
-#' @noRd
-.check_cols_as_expected <- function(expected, df) {
-  observed <- ds.colnames(df)[[1]]
-  expect_equal(observed, expected)
-}
-
 #' Create a DSLite login object that can be used for testing
 #'
 #' @param assign_method A string specifying the name of the custom assign method to be added
