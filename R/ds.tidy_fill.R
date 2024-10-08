7f94a847f03e7759246d711113fc64bf2bd493cc
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-10-08 17:47:24 +0200

    tests: working through sub functions

--- R/ds.tidy_fill.R
+++ R/ds.tidy_fill.R
@@ -1,9 +1,24 @@
+#' Fill DataFrame with Missing Columns and Adjust Classes
+#'
+#' This function fills a given DataFrame by adding missing columns, ensuring consistent column classes, and adjusting factor levels where necessary.
+#' It performs checks to detect class and factor level conflicts and prompts the user for decisions to resolve these conflicts.
+#'
+#' @param df.name Name of the input DataFrame to fill.
+#' @param newobj Name of the new DataFrame object created after filling.
+#' @param datasources Data sources from which to aggregate data. Default is \code{NULL}.
+#'
+#' @import dplyr
+#' @import purrr
+#' @import datashield
+#' @return The filled DataFrame with added columns and adjusted classes or factor levels.
+#' @examples
+#' ds.tidy_fill(df.name = "df1", newobj = "filled_df", datasources = conns)
 ds.tidy_fill <- function(df.name = NULL, newobj = NULL, datasources = NULL) {
   datasources <- .set_datasources(datasources)
 
   assert_that(is.character("df.name"))
   assert_that(is.character("newobj"))
-  # .perform_tidyverse_checks(df.name, newobj, tidy_select, datasources)
+
   col_names <- datashield.aggregate(datasources, call("colnamesDS", df.name))
   .stop_if_cols_identical(col_names)
 
@@ -41,13 +56,28 @@ ds.tidy_fill <- function(df.name = NULL, newobj = NULL, datasources = NULL) {
                       level_conflicts, levels_decision, newobj)
 }
 
+#' Stop If Columns Are Identical
+#'
+#' Checks if the columns in the data frames are identical and throws an error if they are.
+#'
+#' @param col_names A list of column names from different data sources.
+#' @return None. Throws an error if columns are identical.
+#' @importFrom cli cli_abort
 .stop_if_cols_identical <- function(col_names) {
-  are_identical <- Reduce(identical, col_names)
+  are_identical <- all(sapply(col_names, identical, col_names[[1]]))
   if (are_identical) {
     cli_abort("Columns are identical in all data frames: nothing to fill")
   }
 }
 
+#' Get Variable Classes from DataFrame
+#'
+#' Retrieves the class of each variable in the specified DataFrame from different data sources.
+#'
+#' @param df.name Name of the input DataFrame.
+#' @param datasources Data sources from which to aggregate data.
+#' @return A DataFrame containing the variable classes from each data source.
+#' @import dplyr
 .get_var_classes <- function(df.name, datasources) {
   cally <- call("classAllColsDS", df.name)
   classes <- datashield.aggregate(datasources, cally) %>%
@@ -55,6 +85,14 @@ ds.tidy_fill <- function(df.name = NULL, newobj = NULL, datasources = NULL) {
   return(classes)
 }
 
+#' Identify Class Conflicts
+#'
+#' Identifies conflicts in variable classes across different data sources.
+#'
+#' @param classes A DataFrame containing variable classes across data sources.
+#' @return A list of variables that have class conflicts.
+#' @import dplyr
+#' @importFrom purrr map
 .identify_class_conflicts <- function(classes) {
   different_class <- classes |>
     dplyr::select(-server) |>
@@ -64,6 +102,14 @@ ds.tidy_fill <- function(df.name = NULL, newobj = NULL, datasources = NULL) {
   return(out)
 }
 
+#' Prompt User for Class Decision for All Variables
+#'
+#' Prompts the user to resolve class conflicts for all variables.
+#'
+#' @param vars A vector of variable names with class conflicts.
+#' @param all_servers The names of all servers.
+#' @param all_classes The classes of the variables across servers.
+#' @return A vector of decisions for each variable's class.
 prompt_user_class_decision_all_vars <- function(vars, all_servers, all_classes) {
   decisions <- c()
   for (i in 1:length(vars)) {
@@ -72,6 +118,14 @@ prompt_user_class_decision_all_vars <- function(vars, all_servers, all_classes) 
   return(decisions)
 }
 
+#' Prompt User for Class Decision for a Single Variable
+#'
+#' Prompts the user to resolve a class conflict for a single variable.
+#'
+#' @param var The variable name with a class conflict.
+#' @param all_servers The names of all servers.
+#' @param all_classes The classes of the variable across servers.
+#' @return A decision for the variable's class.
 prompt_user_class_decision <- function(var, all_servers, all_classes) {
   cli_alert_warning("`ds.dataFrameFill` requires that all columns have the same class.")
   cli_alert_danger("Column {.strong {var}} has following classes:")
@@ -80,6 +134,13 @@ prompt_user_class_decision <- function(var, all_servers, all_classes) {
   return(ask_question_wait_response_class(var))
 }
 
+#' Check User Response for Class Decision
+#'
+#' Checks the user's input to ensure it is valid for class decisions.
+#'
+#' @param answer The user's input.
+#' @param var The variable name.
+#' @return The user's decision or a recursive prompt for input.
 check_response_class <- function(answer, var) {
   if (answer == "6") {
     cli_abort("Aborted `ds.dataFrameFill`", .call = NULL)
@@ -92,22 +153,39 @@ check_response_class <- function(answer, var) {
   }
 }
 
+#' Ask Question and Wait for Class Response
+#'
+#' Prompts the user with a question and waits for a response related to class decisions.
+#'
+#' @param question The question to ask the user.
+#' @return The user's decision.
 ask_question_wait_response_class <- function(question) {
   ask_question(question)
   answer <- give_prompt()
   return(check_response_class(answer))
 }
 
-give_prompt <- function() {
-  readline()
-}
-
+#' Fix Variable Classes
+#'
+#' Applies the user's class decisions to fix the classes of variables across different data sources.
+#'
+#' @param df.name The name of the DataFrame.
+#' @param different_classes A list of variables with class conflicts.
+#' @param class_decisions The decisions made by the user.
+#' @param newobj The name of the new DataFrame.
+#' @param datasources Data sources from which to aggregate data.
+#' @return None. Updates the DataFrame with consistent variable classes.
 .fix_classes <- function(df.name, different_classes, class_decisions, newobj, datasources) {
   cally <- call("fixClassDS", df.name, names(different_classes), class_decisions)
   datashield.assign(datasources, newobj, cally)
 }
 
-
+#' Get Unique Columns from Data Sources
+#'
+#' Retrieves all unique columns from the data sources.
+#'
+#' @param col_names A list of column names.
+#' @return A vector of unique column names.
 .get_unique_cols <- function(col_names) {
   return(
     unique(
@@ -116,16 +194,40 @@ give_prompt <- function() {
   )
 }
 
+#' Add Missing Columns to DataFrame
+#'
+#' Adds any missing columns to the DataFrame to ensure all columns are present across data sources.
+#'
+#' @param df.name The name of the DataFrame.
+#' @param unique_cols A vector of unique column names.
+#' @param newobj The name of the new DataFrame.
+#' @param datasources Data sources from which to aggregate data.
+#' @return None. Updates the DataFrame with added columns.
 .add_missing_cols_to_df <- function(df.name, unique_cols, newobj, datasources) {
   cally <- call("makeColsSameDS", df.name, unique_cols)
   datashield.assign(datasources, newobj, cally)
 }
 
+#' Summarize Newly Added Columns
+#'
+#' Summarizes the columns that were added to the DataFrame.
+#'
+#' @param newobj The name of the new DataFrame.
+#' @param datasources Data sources from which to aggregate data.
+#' @param col_names A list of column names.
+#' @return A list of added columns.
 .summarise_new_cols <- function(newobj, datasources, col_names) {
   new_names <- datashield.aggregate(datasources, call("colnamesDS", newobj))
   return(.get_added_cols(col_names, new_names))
 }
 
+#' Get Added Columns
+#'
+#' Compares the old and new column names and identifies newly added columns.
+#'
+#' @param old_names A list of old column names.
+#' @param new_names A list of new column names.
+#' @return A list of added column names.
 .get_added_cols <- function(old_names, new_names) {
   list(old_names, new_names) %>%
     pmap(function(.x, .y) {
@@ -133,6 +235,12 @@ give_prompt <- function() {
     })
 }
 
+#' Identify Factor Variables
+#'
+#' Identifies which variables are factors in the DataFrame.
+#'
+#' @param var_classes A DataFrame containing variable classes.
+#' @return A vector of factor variables.
 .identify_factor_vars <- function(var_classes) {
   return(
     var_classes %>%
@@ -141,11 +249,25 @@ give_prompt <- function() {
   )
 }
 
+#' Get Factor Levels from Data Sources
+#'
+#' Retrieves the levels of factor variables from different data sources.
+#'
+#' @param factor_vars A vector of factor variables.
+#' @param newobj The name of the new DataFrame.
+#' @param datasources Data sources from which to aggregate data.
+#' @return A list of factor levels.
 .get_factor_levels <- function(factor_vars, newobj, datasources) {
   cally <- call("getAllLevelsDS", newobj, names(factor_vars))
   return(datashield.aggregate(datasources, cally))
 }
 
+#' Identify Factor Level Conflicts
+#'
+#' Identifies conflicts in factor levels across different data sources.
+#'
+#' @param factor_levels A list of factor levels.
+#' @return A list of variables with level conflicts.
 .identify_level_conflicts <- function(factor_levels) {
   levels <- factor_levels %>%
     pmap_lgl(function(...) {
@@ -156,18 +278,37 @@ give_prompt <- function() {
   return(names(levels[levels == TRUE]))
 }
 
+#' Ask Question and Wait for Response on Factor Levels
+#'
+#' Prompts the user with options for resolving factor level conflicts and waits for a response.
+#'
+#' @param level_conflicts A list of variables with factor level conflicts.
+#' @return The user's decision.
 ask_question_wait_response_levels <- function(level_conflicts) {
   .make_levels_message(level_conflicts)
   answer <- give_prompt()
   return(check_response_levels(answer, level_conflicts))
 }
 
+#' Make Factor Level Conflict Message
+#'
+#' Creates a message to alert the user about factor level conflicts and prompt for action.
+#'
+#' @param level_conflicts A list of variables with factor level conflicts.
+#' @return None. Prints the message to the console.
 .make_levels_message <- function(level_conflicts) {
   cli_alert_warning("Warning: factor variables {level_conflicts} do not have the same levels in all studies")
   cli_alert_info("Would you like to:")
   cli_ol(c("Create the missing levels where they are not present", "Do nothing"))
 }
 
+#' Check User Response for Factor Levels
+#'
+#' Checks the user's input to ensure it is valid for resolving factor level conflicts.
+#'
+#' @param answer The user's input.
+#' @param level_conflicts A list of variables with factor level conflicts.
+#' @return The user's decision.
 check_response_levels <- function(answer, level_conflicts) {
   if (!answer %in% as.character(1:2)) {
     cli_alert_warning("Invalid input. Please try again.")
@@ -178,6 +319,13 @@ check_response_levels <- function(answer, level_conflicts) {
   }
 }
 
+#' Get Unique Factor Levels
+#'
+#' Retrieves the unique factor levels for variables with conflicts.
+#'
+#' @param factor_levels A list of factor levels.
+#' @param level_conflicts A list of variables with level conflicts.
+#' @return A list of unique factor levels.
 .get_unique_levels <- function(factor_levels, level_conflicts) {
   unique_levels <- factor_levels %>%
     map(~ .[level_conflicts]) %>%
@@ -188,11 +336,31 @@ check_response_levels <- function(answer, level_conflicts) {
   return(unique_levels)
 }
 
+#' Set Factor Levels in DataFrame
+#'
+#' Applies the unique factor levels to the DataFrame.
+#'
+#' @param newobj The name of the new DataFrame.
+#' @param unique_levels A list of unique factor levels.
+#' @param datasources Data sources from which to aggregate data.
+#' @return None. Updates the DataFrame with the new factor levels.
 .set_factor_levels <- function(newobj, unique_levels, datasources) {
   cally <- call("setAllLevelsDS", newobj, names(unique_levels), unique_levels)
   datashield.assign(datasources, newobj, cally)
 }
 
+#' Print Out Summary Messages
+#'
+#' Prints summary messages regarding the filled DataFrame, including added columns, class decisions, and factor level adjustments.
+#'
+#' @param added_cols A list of added columns.
+#' @param class_decisions A vector of class decisions.
+#' @param different_classes A list of variables with class conflicts.
+#' @param unique_levels A list of unique factor levels.
+#' @param level_conflicts A list of variables with level conflicts.
+#' @param levels_decision The decision made regarding factor levels.
+#' @param newobj The name of the new DataFrame.
+#' @return None. Prints messages to the console.
 .print_out_messages <- function(added_cols, class_decisions, different_classes, unique_levels,
                                 level_conflicts, levels_decision, newobj) {
   .print_var_recode_message(added_cols, newobj)
@@ -207,6 +375,13 @@ check_response_levels <- function(answer, level_conflicts) {
   }
 }
 
+#' Print Variable Recode Message
+#'
+#' Prints a message summarizing the columns that were added to the DataFrame.
+#'
+#' @param added_cols A list of added columns.
+#' @param newobj The name of the new DataFrame.
+#' @return None. Prints the message to the console.
 .print_var_recode_message <- function(added_cols, newobj) {
   cli_alert_success("The following variables have been added to {newobj}:")
   added_cols_neat <- added_cols %>% map(~ ifelse(length(.) == 0, "", .))
@@ -217,6 +392,14 @@ check_response_levels <- function(answer, level_conflicts) {
   cli_text("")
 }
 
+#' Print Class Recode Message
+#'
+#' Prints a message summarizing the class decisions that were made for variables with conflicts.
+#'
+#' @param class_decisions A vector of class decisions.
+#' @param different_classes A list of variables with class conflicts.
+#' @param newobj The name of the new DataFrame.
+#' @return None. Prints the message to the console.
 .print_class_recode_message <- function(class_decisions, different_classes, newobj) {
   choice_neat <- change_choice_to_string(class_decisions)
   class_message <- paste0(names(different_classes), " --> ", choice_neat)
@@ -226,6 +409,13 @@ check_response_levels <- function(answer, level_conflicts) {
   }
 }
 
+#' Print Factor Levels Recode Message
+#'
+#' Prints a message summarizing the factor level decisions that were made for variables with conflicts.
+#'
+#' @param unique_levels A list of unique factor levels.
+#' @param newobj The name of the new DataFrame.
+#' @return None. Prints the message to the console.
 .print_levels_recode_message <- function(unique_levels, newobj) {
   levels_message <- .make_levels_recode_message(unique_levels)
   cli_alert_success("The following levels have been set in {newobj}: ")
@@ -234,6 +424,12 @@ check_response_levels <- function(answer, level_conflicts) {
   }
 }
 
+#' Make Levels Recode Message
+#'
+#' Creates a message to alert the user about factor level recoding.
+#'
+#' @param unique_levels A list of unique factor levels.
+#' @return A formatted string summarizing the level recoding.
 .make_levels_recode_message <- function(unique_levels) {
   return(
     list(names(unique_levels), unique_levels) %>%
@@ -242,6 +438,7 @@ check_response_levels <- function(answer, level_conflicts) {
       })
   )
 }
+
 
 
 
---
+++ tests/testthat/test-tidy_fill.R
@@ -0,0 +1,138 @@
+library(DSLite)
+library(dplyr)
+library(dsTidyverse)
+library(dsBase)
+library(dsBaseClient)
+library(purrr)
+
+df <- create_mixed_dataframe(n_rows = 100, n_factor_cols = 10, n_other_cols = 10)
+
+df_1 <- df %>% select(1:5, 6, 9, 12, 15, 18) %>%
+  mutate(
+    fac_col2 = factor(fac_col2, levels = c("Blue", "Green")),
+    fac_col4 = as.numeric(fac_col4),
+    fac_col5 = as.logical(fac_col5))
+
+df_2 <- df %>% select(1:5, 7, 10, 13, 16, 19) %>%
+  mutate(
+    fac_col2 = factor(fac_col2, levels = c("Green", "Red")),
+    fac_col3 = factor(fac_col3, levels = "No"),
+    fac_col4 = as.character(fac_col4),
+    fac_col5 = as.integer(fac_col5))
+
+df_3 <- df %>% select(1:5, 11, 14, 17, 20) %>%
+  mutate(
+    fac_col2 = factor(fac_col2, levels = "Blue"),
+    fac_col3 = factor(fac_col3, levels = "Yes"))
+
+options(datashield.env = environment())
+
+dslite.server <- newDSLiteServer(
+  tables = list(
+    df_1 = df_1,
+    df_2 = df_2,
+    df_3 = df_3
+  )
+)
+
+dslite.server$config(defaultDSConfiguration(include = c("dsBase", "dsTidyverse", "dsDanger")))
+dslite.server$aggregateMethod("classAllColsDS", "classAllColsDS")
+
+builder <- DSI::newDSLoginBuilder()
+
+builder$append(
+  server = "server_1",
+  url = "dslite.server",
+  driver = "DSLiteDriver"
+)
+
+builder$append(
+  server = "server_2",
+  url = "dslite.server",
+  driver = "DSLiteDriver"
+)
+
+builder$append(
+  server = "server_3",
+  url = "dslite.server",
+  driver = "DSLiteDriver"
+)
+
+logindata <- builder$build()
+conns <- DSI::datashield.login(logins = logindata, assign = FALSE)
+
+datashield.assign.table(conns["server_1"], "df", "df_1")
+datashield.assign.table(conns["server_2"], "df", "df_2")
+datashield.assign.table(conns["server_3"], "df", "df_3")
+
+test_that(".stop_if_cols_identical throws error if columns are identical", {
+
+  identical_cols <- list(
+    c("col1", "col2", "col3"),
+    c("col1", "col2", "col3"),
+    c("col1", "col2", "col3")
+  )
+
+  expect_error(
+    .stop_if_cols_identical(identical_cols),
+    "Columns are identical in all data frames: nothing to fill"
+  )
+
+})
+
+test_that(".stop_if_cols_identical doesn't throw error if data frames have different columns", {
+
+  different_cols <- list(
+    c("col1", "col2", "col3"),
+    c("col1", "col2", "col4"),
+    c("col1", "col5", "col3")
+  )
+
+  expect_silent(
+    .stop_if_cols_identical(different_cols)
+  )
+
+})
+
+var_class <- .get_var_classes("df", datasources = conns)
+
+test_that(".get_var_classes returns correct output", {
+
+  expected <- tibble(
+    server = c("server_1", "server_2", "server_3"),
+    fac_col1 = c("factor", "factor", "factor"),
+    fac_col2 = c("factor", "factor", "factor"),
+    fac_col3 = c("factor", "factor", "factor"),
+    fac_col4 = c("numeric", "character", "factor"),
+    fac_col5 = c("logical", "integer", "factor"),
+    fac_col6 = c("factor", NA, NA),
+    fac_col9 = c("factor", NA, NA),
+    col12 = c("numeric", NA, NA),
+    col15 = c("integer", NA, NA),
+    col18 = c("logical", NA, NA),
+    fac_col7 = c(NA, "factor", NA),
+    fac_col10 = c(NA, "factor", NA),
+    col13 = c(NA, "character", NA),
+    col16 = c(NA, "numeric", NA),
+    col19 = c(NA, "integer", NA),
+    col11 = c(NA, NA, "integer"),
+    col14 = c(NA, NA, "logical"),
+    col17 = c(NA, NA, "character"),
+    col20 = c(NA, NA, "numeric")
+  )
+
+  expect_equal(var_class, expected)
+
+})
+
+class_conflicts <- .identify_class_conflicts(var_classes)
+
+test_that(".identify_class_conflicts returns correct output", {
+  expected <- list(
+    fac_col4 = c("numeric", "character", "factor"),
+    fac_col5 = c("logical", "integer", "factor")
+  )
+
+  expect_equal(class_conflicts, expected)
+
+})
