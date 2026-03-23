3356cd64eabeffca48cfcda663b2e99f24a93b14
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2026-03-23 16:47:09 +0100

    refactor: move tidyselect helper tests to test-select-permitted-functions.R

--- tests/testthat/test-select.R
+++ tests/testthat/test-select.R
@@ -5,11 +5,9 @@ require(dsTidyverse)
 require(dsBaseClient)
 
 data("mtcars")
-num_range_df <- data.frame(x1 = 1:5, x2 = 6:10, x3 = 11:15, y1 = 16:20, stringsAsFactors = FALSE)
-login_data <- .prepare_dslite(assign_method = "selectDS", tables = list(mtcars = mtcars, num_range_df = num_range_df))
+login_data <- .prepare_dslite(assign_method = "selectDS", tables = list(mtcars = mtcars))
 conns <- datashield.login(logins = login_data)
 datashield.assign.table(conns, "mtcars", "mtcars")
-datashield.assign.table(conns, "num_range_df", "num_range_df")
 
 test_that("ds.select fails with correct error message if data not present ", {
   skip_if_not_installed("dsBaseClient")
@@ -35,96 +33,6 @@ test_that("ds.select correctly passes : ", {
   expect_equal(
     ds.colnames("mpg_drat", datasources = conns)[[1]],
     c("mpg", "cyl", "disp", "hp", "drat")
-  )
-})
-
-test_that("ds.select correctly passes `starts_with`", {
-  skip_if_not_installed("dsBaseClient")
-  ds.select(
-    df.name = "mtcars",
-    tidy_expr = list(starts_with("m")),
-    newobj = "starts",
-    datasources = conns
-  )
-
-  expect_equal(
-    ds.colnames("starts", datasources = conns)[[1]],
-    "mpg"
-  )
-})
-
-test_that("ds.select correctly passes `ends_with`", {
-  skip_if_not_installed("dsBaseClient")
-  ds.select(
-    df.name = "mtcars",
-    tidy_expr = list(ends_with("m")),
-    newobj = "ends",
-    datasources = conns
-  )
-
-  expect_equal(
-    ds.colnames("ends", datasources = conns)[[1]],
-    "am"
-  )
-})
-
-test_that("ds.select correctly passes `matches`", {
-  skip_if_not_installed("dsBaseClient")
-  ds.select(
-    df.name = "mtcars",
-    tidy_expr = list(matches("a")),
-    newobj = "matches",
-    datasources = conns
-  )
-
-  expect_equal(
-    ds.colnames("matches", datasources = conns)[[1]],
-    c("drat", "am", "gear", "carb")
-  )
-})
-
-test_that("ds.select correctly passes `everything`", {
-  skip_if_not_installed("dsBaseClient")
-  ds.select(
-    df.name = "mtcars",
-    tidy_expr = list(everything()),
-    newobj = "everything",
-    datasources = conns
-  )
-
-  expect_equal(
-    ds.colnames("everything", datasources = conns)[[1]],
-    colnames(mtcars)
-  )
-})
-
-test_that("ds.select correctly passes `last_col`", {
-  skip_if_not_installed("dsBaseClient")
-  ds.select(
-    df.name = "mtcars",
-    tidy_expr = list(last_col()),
-    newobj = "last",
-    datasources = conns
-  )
-
-  expect_equal(
-    ds.colnames("last", datasources = conns)[[1]],
-    "carb"
-  )
-})
-
-test_that("ds.select correctly passes `group_cols`", {
-  skip_if_not_installed("dsBaseClient")
-  ds.select(
-    df.name = "mtcars",
-    tidy_expr = list(group_cols()),
-    newobj = "group",
-    datasources = conns
-  )
-
-  expect_equal(
-    ds.colnames("group", datasources = conns)[[1]],
-    character(0)
   )
 })
 
@@ -173,36 +81,6 @@ test_that("ds.select correctly passes strings with '|'", {
   )
 })
 
-test_that("ds.select correctly passes `strings with `all_of`", {
-  skip_if_not_installed("dsBaseClient")
-  ds.select(
-    df.name = "mtcars",
-    tidy_expr = list(all_of(c("mpg", "cyl"))),
-    newobj = "all_of",
-    datasources = conns
-  )
-
-  expect_equal(
-    ds.colnames("all_of", datasources = conns)[[1]],
-    c("mpg", "cyl")
-  )
-})
-
-test_that("ds.select correctly passes strings with `any_of`", {
-  skip_if_not_installed("dsBaseClient")
-  ds.select(
-    df.name = "mtcars",
-    tidy_expr = list(any_of(c("mpg", "cyl"))),
-    newobj = "any_of",
-    datasources = conns
-  )
-
-  expect_equal(
-    ds.colnames("any_of", datasources = conns)[[1]],
-    c("mpg", "cyl")
-  )
-})
-
 test_that("ds.select correctly passes complex strings", {
   skip_if_not_installed("dsBaseClient")
   ds.select(
@@ -218,21 +96,6 @@ test_that("ds.select correctly passes complex strings", {
   )
 })
 
-test_that("ds.select correctly passes strings with `where`", {
-  skip_if_not_installed("dsBaseClient")
-  ds.select(
-    df.name = "mtcars",
-    tidy_expr = list(where(is.numeric)),
-    newobj = "where",
-    datasources = conns
-  )
-
-  expect_equal(
-    ds.colnames("where", datasources = conns)[[1]],
-    colnames(mtcars)
-  )
-})
-
 test_that("ds.select correctly passes strings with '='", {
   skip_if_not_installed("dsBaseClient")
   ds.select(
@@ -245,20 +108,5 @@ test_that("ds.select correctly passes strings with '='", {
   expect_equal(
     ds.colnames("equals", datasources = conns)[[1]],
     c("test", "cyl", "gear")
-  )
-})
-
-test_that("ds.select correctly passes `num_range`", {
-  skip_if_not_installed("dsBaseClient")
-  ds.select(
-    df.name = "num_range_df",
-    tidy_expr = list(num_range("x", 1:2)),
-    newobj = "num_range_res",
-    datasources = conns
-  )
-
-  expect_equal(
-    ds.colnames("num_range_res", datasources = conns)[[1]],
-    c("x1", "x2")
   )
 })
